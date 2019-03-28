513eab405f85047e89318a321b185960372cc4a0
= <kuslanskyg@gmail.com>
2019-03-27 21:53:42 -0400

    added ux for loading create level

--- src/public/js/createLevel.js
+++ src/public/js/createLevel.js
@@ -1,6 +1,8 @@
 let levelStructure = {"index":0,"levelStructure":{"obstacles":[{"name":"BoxObstacle","x":150,"y":250}],"width":600,"height":600,"spawnCoordinate":{"x":300,"y":500},"populationSize":50,"lifespan":150,"target":{"x":300,"y":50,"r":40}},"metadata":{"views":0,"author":"someone","highestCompletionTime":0},"_id":"paG4iuqeBLgFczw8","createdAt":{"$$date":1553610336655},"updatedAt":{"$$date":1553610336655}}.levelStructure;
 const editing = true;
-const saveLevel = () => {
+const saveLevel = async () => {
    const levelStructure = level.serialize();
-    axios.post('/level', levelStructure);
+    await axios.post('/level', levelStructure);
+    $('#createLevelBtn').attr('disabled', 'disabled').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...')
+      window.location.href = '/';
 }
