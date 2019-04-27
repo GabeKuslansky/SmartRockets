3531a4d9ff4b7ac8c49f25a59509bde8aad9a059
Unknown <joshm1238@gmail.com>
2019-04-27 19:31:42 -0400

    added back black hole

--- src/public/js/canvas/editor.js
+++ src/public/js/canvas/editor.js
@@ -36,7 +36,7 @@ function Editor(width, height){
 	this.previousSelectedObject = null;
 
 
-	this.obstacleList = [Target, SpawnPoint, Rectangle, Circle, Polygon];
+	this.obstacleList = [Target, SpawnPoint, Rectangle, Circle, Polygon, BlackHole];
 	this.selectedIndex = 0;
 
 	this.scaleX = document.getElementById("scaleXForm");
--- src/public/js/canvas/obstacles.js
+++ src/public/js/canvas/obstacles.js
@@ -309,3 +309,131 @@ Circle.draw = function(x, y){
 	circle(x, y, Circle.defaultRadius);
 	pop();
 }
+
+
+
+
+//Black Hole
+function BlackHole(x, y, innerR, outerR){
+	this.type = "BlackHole";
+	this.position = createVector(x, y);
+	if(!innerR){
+		this.innerR = BlackHole.defaultInnerRadius;
+		this.outerR = BlackHole.defaultOuterRadius;
+	}
+	else{
+		this.innerR = innerR;
+		this.outerR = outerR;
+	}
+
+	this.startPosition = createVector(x, y);
+	this.startForce = createVector(0, 0);
+
+	this.scale = createVector(1, 1);
+	this.rotation = 0; //degrees
+	this.step = 0;
+	this.rotationPoint = createVector(0, 0);
+	this.physics = new PhysicsObject(this.position, createVector(1, 1), false, this, false);
+	this.physics.addColliderCircle(0, 0, this.innerR);
+}
+BlackHole.prototype.update = function(){
+	this.rotation += this.step;
+	this.position.sub(this.rotationPoint);
+	this.position.rotate(radians(this.step));
+	this.position.add(this.rotationPoint);	
+
+	let rockets = level.population.rockets;
+	for(let i = 0; i < rockets.length; i++){
+		if(dist(this.position.x, this.position.y, rockets[i].position.x, rockets[i].position.y) < this.outerR*this.scale.x){
+			if(rockets[i].physics != null)
+				rockets[i].physics.applyForce(createVector(this.position.x - rockets[i].position.x, this.position.y - rockets[i].position.y).normalize().mult(BlackHole.force)); //apply force inward to circle
+		}
+	}
+}
+BlackHole.prototype.draw = function(x, y){
+	push();
+	if(!x){
+		translate(this.position.x, this.position.y);
+		rotate(radians(this.rotation));
+		translate(-this.position.x, -this.position.y);
+		push();
+		fill(0, 0, 0, 100);
+		circle(this.position.x, this.position.y, this.outerR*this.scale.x);
+		fill(0, 0, 0, 200);
+		circle(this.position.x, this.position.y, this.innerR);
+		pop();
+	}
+	else{
+		translate(x, y);
+		rotate(radians(this.rotation));
+		translate(-x, -y);
+		push();
+		fill(0, 0, 0, 100);
+		circle(x, y, this.outerR*this.scale.x);
+		fill(0, 0, 0, 200);
+		circle(x, y, this.innerR);
+		pop();
+
+	}
+	pop();
+}
+BlackHole.prototype.setRotation = function(angle){
+	//undo current rotation
+	this.position.sub(this.rotationPoint);
+	this.position.rotate(radians(-this.rotation));
+	this.position.add(this.rotationPoint);
+	//rotate
+	this.rotation = angle;
+	this.position.sub(this.rotationPoint);
+	this.position.rotate(radians(this.rotation));
+	this.position.add(this.rotationPoint);
+}
+//Delete
+BlackHole.prototype.deleteObstacle = function(){
+	//Delete physics
+	this.physics.deletePhysics();
+	//Remove from obstacles
+	let deleted = false;
+	for(let i = 0; i < level.obstacles.length && !deleted; i++){
+			if(this == level.obstacles[i]){
+				level.obstacles.splice(i, 1);
+				deleted = true;
+			}
+	}
+}
+//Start
+BlackHole.prototype.start = function(){
+	this.physics.applyForce(this.startForce);
+}
+//Reset
+BlackHole.prototype.reset = function(){
+	this.position.x = this.startPosition.x;
+	this.position.y = this.startPosition.y;
+	this.physics.velocity.x = 0;
+	this.physics.velocity.y = 0;
+	this.physics.acceleration.x = 0;
+	this.physics.acceleration.y = 0;
+	this.rotation = 0;
+}
+BlackHole.defaultInnerRadius = 10;
+BlackHole.defaultOuterRadius = 55;
+BlackHole.force = 1.5;
+
+//Where xy is the cneter
+BlackHole.drawToGraphics = function(pg, x, y){
+	pg.push();
+	pg.fill(0, 0, 0, 100);
+	pg.circle(x, y, BlackHole.defaultOuterRadius/1.2);
+	pg.fill(0, 0, 0, 200);
+	pg.circle(x, y, BlackHole.defaultInnerRadius/1.2);
+	pg.pop();
+}
+//Draw with default values
+BlackHole.draw = function(x, y){
+	push();
+	fill(0, 0, 0, 100);
+	circle(x, y, BlackHole.defaultOuterRadius);
+	fill(0, 0, 0, 200);
+	circle(x, y, BlackHole.defaultInnerRadius);
+	pop();
+}
